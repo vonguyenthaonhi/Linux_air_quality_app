fdbe7e4205de91a0acfe2babc5d153836a8fc8f4
Vo Nguyen Thao Nhi <vonguyenthaonhi26062002@gmail.com>
2024-12-08 23:04:46 +0100

    70 characters

--- data_collector/get/get_data.sh
+++ data_collector/get/get_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-URL_AIR="https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/openaq/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B"
+URL_AIR="https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets\
+/openaq/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&\
+delimiter=%3B"
 URL_CITIES="https://datahub.io/core/world-cities/r/world-cities.csv"
-
 
 OUTPUT_PATH_AIR="../../openaq_data.csv"
 OUTPUT_PATH_CITIES="../../world_cities.csv"
-
 
 if [ ! -d "$(dirname "$OUTPUT_PATH_AIR")" ]; then
     echo "Erreur : Le répertoire $(dirname "$OUTPUT_PATH_AIR") n'existe pas."
@@ -18,22 +18,18 @@ if [ ! -d "$(dirname "$OUTPUT_PATH_CITIES")" ]; then
     exit 1
 fi
 
-
-echo "Téléchargement des données de la qualité de l'air depuis : $URL_AIR"
+echo "Téléchargement des données de l'air depuis : $URL_AIR"
 curl -L -o "$OUTPUT_PATH_AIR" "$URL_AIR" || {
-    echo "Erreur : Le téléchargement des données de qualité de l'air a échoué."
+    echo "Erreur : Le téléchargement des données de l'air a échoué."
     exit 1
 }
 
-echo "Téléchargement des données de qualité de l'air terminé. Les données ont été enregistrées dans '$OUTPUT_PATH_AIR'."
+echo "Téléchargement terminé. Données enregistrées dans '$OUTPUT_PATH_AIR'."
 
-
-echo "Téléchargement des données des villes mondiales depuis : $URL_CITIES"
+echo "Téléchargement des données des villes depuis : $URL_CITIES"
 curl -L -o "$OUTPUT_PATH_CITIES" "$URL_CITIES" || {
-    echo "Erreur : Le téléchargement des données des villes mondiales a échoué."
+    echo "Erreur : Le téléchargement des données des villes a échoué."
     exit 1
 }
 
-echo "Téléchargement des données des villes mondiales terminé. Les données ont été enregistrées dans '$OUTPUT_PATH_CITIES'."
-
-
+echo "Téléchargement terminé. Données enregistrées dans '$OUTPUT_PATH_CITIES'."
--- data_processor/core.py
+++ data_processor/core.py
@@ -6,8 +6,7 @@ raw_path_air = "../openaq_data.csv"
 world_cities_path = "../world_cities.csv"
 processed_file_path = "../processed_data.csv"
 
-print(f'Téléchargement de la base de données air quality à: {raw_path_air}')
-
+print(f'Téléchargement de la base air quality à: {raw_path_air}')
 
 try:
     data = pd.read_csv(
@@ -17,21 +16,21 @@ try:
         skipinitialspace=True,  
         encoding='utf8'  
     )
-    print("Données sur la qualité de l'air importées avec succès.")
+    print("Données qualité de l'air importées avec succès.")
 except Exception as e:
-    print(f"Erreur lors de la lecture des données sur la qualité de l'air : {e}")
+    print(f"Erreur lors de la lecture des données qualité de l'air : {e}")
     exit(1)
 
-print(f'Téléchargement de la base de données des villes à: {world_cities_path}')
+print(f'Téléchargement de la base des villes à: {world_cities_path}')
 
 try:
     df_valid_cities = pd.read_csv(
         world_cities_path,
         encoding='utf8'  
     )
-    print("Données des villes mondiales importées avec succès.")
+    print("Données des villes importées avec succès.")
 except Exception as e:
-    print(f"Erreur lors de la lecture des données des villes mondiales : {e}")
+    print(f"Erreur lors de la lecture des données des villes : {e}")
     exit(1)
 
 #### Traitements des données
@@ -41,22 +40,22 @@ if 'Coordinates' in data.columns:
     if coord_data.shape[1] == 2:
         data[['Latitude', 'Longitude']] = coord_data.astype(float)
     else:
-        print("Erreur : les coordonnées ne peuvent pas être séparées correctement. Veuillez vérifier le format.")
+        print("Erreur : format des coordonnées incorrect. Vérifiez le format.")
         exit(1)
 else:
     print("Erreur : la colonne 'Coordinates' est manquante.")
     exit(1)
 
-
 data = data.dropna(subset=['Latitude', 'Longitude'])
-data=data.drop(columns="Coordinates")
-data=data.dropna(subset=['City','Country Label'])
-data=data[data['Unit']!="ppm"]
+data = data.drop(columns="Coordinates")
+data = data.dropna(subset=['City', 'Country Label'])
+data = data[data['Unit'] != "ppm"]
 data = data[data['Value'] >= 0]
 
-data ["Last Updated"] = data ["Last Updated"].astype(str)
-data ["Last Updated"] = data ["Last Updated"].str[:10]
-data ["Last Updated"] = pd.to_datetime(data["Last Updated"], format='%Y-%m-%d', errors='coerce')
+data["Last Updated"] = data["Last Updated"].astype(str).str[:10]
+data["Last Updated"] = pd.to_datetime(
+    data["Last Updated"], format='%Y-%m-%d', errors='coerce'
+)
 data = data[data['Last Updated'].dt.year == 2024]
 
 data['City'] = data['City'].astype(str).str.title()
@@ -64,43 +63,31 @@ valid_cities = set(df_valid_cities['name'].str.strip().str.title())
 data['City'] = data['City'].str.strip().str.title()
 data = data[data['City'].isin(valid_cities)]
 
-
 ##### Conversion des unités de concentration
 molar_masses = {
-    'O3': 48,     
-    'CO': 28,     
-    'NO2': 46,    
-    'SO2': 64,    
-    'PM10': None, 
-    'PM2.5': None,
-    'NO': 30,     
-    'NOX': 38     
+    'O3': 48, 'CO': 28, 'NO2': 46, 'SO2': 64,
+    'PM10': None, 'PM2.5': None, 'NO': 30, 'NOX': 38
 }
 
-print("Convertit les unités de concentration en µg/m³ pour comparaison...")
+print("Convertit les unités en µg/m³ pour comparaison...")
 
 def convert_to_ugm3(row):
     """
-    Convertit les unités de concentration en µg/m³ et met à jour l'unité correspondante.
+    Convertit les unités en µg/m³ et met à jour l'unité correspondante.
     """
     unit = row['Unit']
     pollutant = row['Pollutant']
     value = row['Value']
-    
-    
     molar_mass = molar_masses.get(pollutant, None)
     
     if unit == 'µg/m³':
         return value, 'µg/m³'
-    
     elif unit == 'ppm' and molar_mass:
-        converted_value = value * (molar_mass / 24.45)  
+        converted_value = value * (molar_mass / 24.45)
         return converted_value, 'µg/m³'
-
     elif unit == 'ppb' and molar_mass:
-        converted_value = value * (molar_mass / 24.45) / 1000 
+        converted_value = value * (molar_mass / 24.45) / 1000
         return converted_value, 'µg/m³'
-
     else:
         return value, unit 
 
@@ -108,10 +95,9 @@ data[['Value', 'Unit']] = data.apply(
     lambda row: pd.Series(convert_to_ugm3(row)), axis=1
 )
 
-
 ##### Exportation des données
 try:
     data.to_csv(processed_file_path, index=False)
-    print(f"Données traitées sauvegardées dans {processed_file_path}")
+    print(f"Données sauvegardées dans {processed_file_path}")
 except Exception as e:
-    print(f"Erreur lors de la sauvegarde des données traitées : {e}")
+    print(f"Erreur lors de la sauvegarde des données : {e}")
