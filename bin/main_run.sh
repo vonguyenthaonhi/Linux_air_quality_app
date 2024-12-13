8f1e375d515e88035e2bc168b53603b7eb5d9ace
chekalisa <chalisa2002@mail.ru>
2024-12-13 14:13:29 +0100

    Update main_run.sh

--- bin/main_run.sh
+++ bin/main_run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Récupérer le répertoire du script principal
 BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 echo "Chemin base: $BASE_DIR"
 
-
-# Liste des chemins des scripts à exécuter
 scripts=(
     "$BASE_DIR/../data_collector/bin/run.sh"
     "$BASE_DIR/../data_processor/bin/run.sh"
     "$BASE_DIR/../webapp/bin/run.sh"
 )
 
-# Exécuter chaque script
 for script in "${scripts[@]}"; do
     if [[ -x "$script" ]]; then
         echo "Running $script..."
